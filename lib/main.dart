70d6892565dd6e4ecc3b60f3236a24ce33f8fff0
maulinadwi <maulinadwi04@gmail.com>
2025-09-09 22:37:54 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
+import 'pages/item_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const BelanjaApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BelanjaApp extends StatelessWidget {
+  const BelanjaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shopping List',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePage(),
+      title: 'Aplikasi Belanja',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/item': (context) => const ItemPage(),
+      },
     );
   }
 }
