b73496428c73aff757771ccbe9f5f0d1e4cf7d78
maulinadwi <maulinadwi04@gmail.com>
2025-09-09 22:39:06 +0700

    Update item_page.dart

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -2,17 +2,58 @@ import 'package:flutter/material.dart';
 import '../models/item.dart';
 
 class ItemPage extends StatelessWidget {
-  final Item item;
-  const ItemPage({super.key, required this.item});
+  const ItemPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final item = ModalRoute.of(context)!.settings.arguments as Item;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Shopping List"),
-      ),
-      body: Center(
-        child: Text("${item.name} with ${item.price}"),
+      appBar: AppBar(title: Text(item.name)),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Hero(
+              tag: 'itemPhoto${item.name}',
+              child: Image.network(
+                item.photo,
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.name,
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "Harga: Rp ${item.price}",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      const Icon(Icons.star, color: Colors.amber),
+                      Text(item.rating.toString()),
+                      const SizedBox(width: 16),
+                      Text("Stok: ${item.stock}"),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                      "Ini adalah deskripsi produk yang menarik. Produk ini sangat berkualitas dan cocok untuk Anda, Produk kebutuhan pokok sehari-hari dengan kualitas terjamin"),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
